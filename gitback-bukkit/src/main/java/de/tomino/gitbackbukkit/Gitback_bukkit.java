0fe6ea77fc6443d4cf3812f9b41227e42da2f5bc
TominoLP <erfinder.tom@gmail.com>
2023-12-20 10:56:53 +0100

    init

---
+++ gitback-bukkit/src/main/java/de/tomino/gitbackbukkit/Gitback_bukkit.java
@@ -0,0 +1,17 @@
+package de.tomino.gitbackbukkit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Gitback_bukkit extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
